9c33e52f7cea0c88e76238f484254aa688d810a8
developer0793 <aurixisintellix@gmail.com>
2026-02-09 12:17:54 +0530

    python program for multiline message display

---
+++ Displaying Multiline message/multi_line_message.py
@@ -0,0 +1,10 @@
+#comments
+''' This file contains a multiline message
+Then we will display it '''
+#multiline string
+msg =''' Hello how are you.
+I am Fine.
+What about you?
+I am fine and learning python.'''
+#to display it
+print(msg)
